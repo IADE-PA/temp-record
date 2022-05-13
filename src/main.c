8c8baab7c82578a3f75c961d3d9cf17e2329336d
amgs <amgs>
2022-05-13 17:56:13 +0100

    LR done.

--- src/main.c
+++ src/main.c
@@ -2,13 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+ 
 #include "list_mm.h"
 
 #define _POSIX_C_SOURCE 200809L
 
 bool equal_elements(Element* first, Element* second) {
     return strcmp(first->date, second->date) == 0;
+}
+
+void replace_char(char* str,  int length, char char_to_replace, char replacement){
+    for(int i = 0; i < length; i++){
+        if(str[i] == char_to_replace){
+            str[i] = replacement;
+        }
+    }
 }
 
 int main() {
@@ -21,11 +29,14 @@ int main() {
     char* line = NULL;
     size_t line_chars = 0;
     while (true) {
-        getline(&line, &line_chars, stdin);
-        if (strcmp(line, "\n") == 0) {
+        int size = getline(&line, &line_chars, stdin);
+        replace_char(line, size, '\n','\0');
+        
+        if (strcmp(line, "") == 0) {
             break;
         }
         char* command = strtok(line, " ");
+        
         if (strcmp(command, "AR") == 0) {
             char* date = strtok(NULL, " ");
             char* temperature = strtok(NULL, " ");
@@ -47,12 +58,15 @@ int main() {
             // * Mostrar a temperatura para a data indicada
             if (idx > 0) {
                 Element target = list_get(list, idx);
-                printf("A temperatura em %s foi %f\n", target.date, target.temperature);
+                printf("A temperatura em %s foi %1.2f\n", target.date, target.temperature);
             } else {
                 printf("Data sem registo.\n");
             }
         } else if (strcmp(command, "LR") == 0) {
-            
+            for(size_t i=0; i<list_size(list); i++) {
+                Element element = list_get(list, i);
+                printf("%s -> %1.2f\n", element.date, element.temperature);
+            }
         }
     }
 
